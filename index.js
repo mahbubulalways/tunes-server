0d505cd676e3eb0d1a376ba63e61348c48f19c52
mahbubulalways <mahbubulhasan604@gmail.com>
2023-06-11 14:59:47 +0600

    vercel problem solve

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const verifyJwt=(req,res,next)=>{
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+     client.connect();
 
     const ClassesCollection = client.db("tunes").collection("classes");
     const instructorCollection = client.db("tunes").collection("instructors");
